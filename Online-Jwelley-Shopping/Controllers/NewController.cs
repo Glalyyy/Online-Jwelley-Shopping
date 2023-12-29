f7a3f39c11dea6bff3ff6a5d92f3fd18b76a7a98
Glalyyy <gamcth10a2.c3truonglongtay@gmail.com>
2023-12-29 10:12:59 +0700

    3.0.0.0

---
+++ Online-Jwelley-Shopping/Controllers/NewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jewelly.Controllers
+{
+    public class NewController : Controller
+    {
+        // GET: New
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult SizeRing()
+        {
+            return View();
+        }
+        public ActionResult Pearl()
+        {
+            return View();
+        }
+        public ActionResult Lab()
+        {
+            return View();
+        }
+        public ActionResult PinkDia()
+        {
+            return View();
+        }
+        public ActionResult ColorDia()
+        {
+            return View();
+        }
+        public ActionResult Preserve()
+        {
+            return View();
+        }
+    }
+}
